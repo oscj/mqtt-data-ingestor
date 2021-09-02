d4de40c740d7767a5ea78dce35939d0eef6dbea5
oscjaimes <oscar@jaimes.io>
2021-09-01 19:36:37 -0600

    refactored client into separate file, no longer using global config. now returning client handler functions from wrapper functions

---
+++ client.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+// subscribes client to target topic
+func sub(client mqtt.Client, targetTopic string, msgHandler mqtt.MessageHandler) {
+	token := client.Subscribe(targetTopic, 1, msgHandler)
+	token.Wait()
+	fmt.Printf("Subscribed to topic: %s", targetTopic)
+}
+
+// client handlers
+func MakeConnectedHandler(config Config) func(mqtt.Client) {
+	return func(client mqtt.Client) {
+		fmt.Println("Connected")
+		sub(client, config.TargetTopic, MakeMessageHandler(config))
+	}
+}
+
+func MakeConnectionLostHandler() func(mqtt.Client, error) {
+	return func(client mqtt.Client, err error) {
+		fmt.Printf("Connection lost: %v\n", err)
+	}
+}
+
+func MakeMessageHandler(config Config) func(mqtt.Client, mqtt.Message) {
+	return func(client mqtt.Client, msg mqtt.Message) {
+		fmt.Printf("Received message: %s from topic: %s\nInserting Message...\n", msg.Payload(), msg.Topic())
+
+		var bSensorDoc interface{}
+		err := bson.UnmarshalExtJSON(msg.Payload(), true, &bSensorDoc)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		dbClient, err := get_client(config.DBUri)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = dbClient.insertDocToCollection(
+			config.TargetDB,
+			config.TargetCollection,
+			bSensorDoc,
+		)
+
+		if err != nil {
+			fmt.Println("Error inserting incoming msg to db")
+		} else {
+			fmt.Println("Successfully inserted incoming msg to db")
+		}
+
+		dbClient.disconnect()
+	}
+}
--- ingestor.go
+++ ingestor.go
@@ -6,7 +6,6 @@ import (
 	"os"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
-	"go.mongodb.org/mongo-driver/bson"
 	"gopkg.in/yaml.v2"
 )
 
@@ -22,8 +21,6 @@ type Config struct {
 	TargetCollection string `yaml:"target_collection"`
 }
 
-var config Config
-
 func main() {
 
 	cfgData, err := os.ReadFile("config.yaml")
@@ -32,7 +29,7 @@ func main() {
 	}
 
 	// default config
-	config = Config{
+	config := Config{
 		BrokerPort:       1883,
 		TargetDB:         "test",
 		TargetCollection: "test",
@@ -51,8 +48,8 @@ func main() {
 	opts.SetClientID(config.ClientId)
 	opts.SetUsername(config.ClientUserName)
 	opts.SetPassword(config.ClientPassword)
-	opts.OnConnect = connectedHandler
-	opts.OnConnectionLost = connectionLostHandler
+	opts.OnConnect = MakeConnectedHandler(config)
+	opts.OnConnectionLost = MakeConnectionLostHandler()
 	client := mqtt.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
@@ -62,52 +59,3 @@ func main() {
 	for {
 	}
 }
-
-// subscribes client to target topic
-func sub(client mqtt.Client, targetTopic string, msgHandler mqtt.MessageHandler) {
-	token := client.Subscribe(targetTopic, 1, msgHandler)
-	token.Wait()
-	fmt.Printf("Subscribed to topic: %s", targetTopic)
-}
-
-// client handlers
-var (
-	connectedHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
-		fmt.Println("Connected")
-		sub(client, config.TargetTopic, messagePubHandler)
-	}
-
-	connectionLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
-		fmt.Printf("Connection lost: %v\n", err)
-	}
-
-	messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-		fmt.Printf("Received message: %s from topic: %s\nInserting Message...\n", msg.Payload(), msg.Topic())
-
-		var bSensorDoc interface{}
-		err := bson.UnmarshalExtJSON(msg.Payload(), true, &bSensorDoc)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		dbClient, err := get_client(config.DBUri)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = dbClient.insertDocToCollection(
-			config.TargetDB,
-			config.TargetCollection,
-			bSensorDoc,
-		)
-
-		if err != nil {
-			fmt.Println("Error inserting incoming msg to db")
-		} else {
-			fmt.Println("Successfully inserted incoming msg to db")
-		}
-
-		dbClient.disconnect()
-	}
-)
